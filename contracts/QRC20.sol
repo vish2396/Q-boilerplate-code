940fd856941680e4d23f87850425e3bf1e9b4c99
vish2396 <vishwajayaneththi@gmail.com>
2024-07-19 10:58:19 +0530

    update code

--- contracts/QRC20.sol
+++ contracts/QRC20.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity 0.8.19;
 
+// Import necessary contracts
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-
-
 import "@q-dev/gdk-contracts/interfaces/tokens/IQRC20.sol";
 import "@q-dev/gdk-contracts/metadata/ContractMetadata.sol";
 
 /**
  * @title QRC20
  *
- * Regular ERC20 token with additional features:
+ * Regular QRC20 token with additional features:
  * - minting and burning
  * - total supply cap
  * - contract metadata
  */
 contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable{
-    string public QRC20_RESOURCE;
-    uint256 public totalSupplyCap;
-    uint8 internal _decimals;
+    string public QRC20_RESOURCE; // Resource of the QRC20 token
+    uint256 public totalSupplyCap; // Maximum total supply cap of the token
+    uint8 internal _decimals; // Number of decimals for the token
 
+    // Initialize the contract with provided metadata and configuration
     function initialize(
         string calldata name_,
         string calldata symbol_,
@@ -29,26 +29,29 @@ contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable
         string calldata resource_,
         uint256 totalSupplyCap_
     ) public initializer {
-        __ERC20_init(name_, symbol_);
-        __ContractMetadata_init(contractURI_);
-        QRC20_RESOURCE = resource_;
-        _decimals = decimals_;
-        totalSupplyCap = totalSupplyCap_;
+        __ERC20_init(name_, symbol_); // Initialize ERC20 token with name and symbol
+        __ContractMetadata_init(contractURI_); // Initialize contract metadata with URI
+        QRC20_RESOURCE = resource_; // Set QRC20 resource
+        _decimals = decimals_; // Set token decimals
+        totalSupplyCap = totalSupplyCap_; // Set total supply cap
 
-        //Set Owner
+        // Set contract owner
         __Ownable_init();
     }
 
+    // Modifier to restrict functions that can change metadata only to the owner
     modifier onlyChangeMetadataPermission() override {
         _checkOwner();
         _;
     }
 
+    // Mint new tokens to the specified account (onlyOwner)
     function mintTo(address account, uint256 amount) external override onlyOwner {
         require(totalSupplyCap == 0 || totalSupply() + amount <= totalSupplyCap, "[QGDK-015000]-The total supply capacity exceeded, minting is not allowed.");
         _mint(account, amount);
     }
 
+    // Burn tokens from the specified account (either self or with allowance)
     function burnFrom(address account, uint256 amount) external override {
         if (account != msg.sender) {
             _spendAllowance(account, msg.sender, amount);
@@ -56,6 +59,7 @@ contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable
         _burn(account, amount);
     }
 
+    // Get the number of decimals for the token
     function decimals() public view override returns (uint8) {
         return _decimals;
     }
--- scripts/AirDropV1/1_deploy_token.js
+++ scripts/AirDropV1/1_deploy_token.js
@@ -1,10 +1,11 @@
+// Import necessary modules
 const { ethers, upgrades } = require("hardhat");
 Web3 = require('web3');
 
-
+// Asynchronous function to deploy QRC20 token
 async function main() {
     
-    //Metadata
+    // Set metadata for the QRC20 token
     const name = "Meta DAO";
     const symbol = "Meta";
     const decimals = 18; // or any other value you want
@@ -12,14 +13,24 @@ async function main() {
     const resource = "QRC20_RESOURCE";
     const totalSupplyCap = Web3.utils.toWei('1000000000', 'ether');
 
-    console.log("Deploying QRC20")
+    // Display deployment message
+    console.log("Deploying QRC20");
+
+    // Get the QRC20 contract factory
     const QRC20 = await ethers.getContractFactory("QRC20");
+
+    // Deploy an upgradeable QRC20 contract using the factory
     const qrc = await upgrades.deployProxy(QRC20, [name,symbol,decimals,contractURI,resource,totalSupplyCap], {initializer:"initialize", kind: "transparent"});
 
+    // Wait for the deployment process to complete
     await qrc.waitForDeployment();
+
+    // Get the address of the deployed QRC20 contract
     const qrc20Address = await qrc.getAddress();
+
+    // Log the deployment address
     console.log("QRC20 deployed to:", qrc20Address);
+}
 
-  } 
-
-  main();
+// Call the main function to deploy the QRC20 token
+main();
